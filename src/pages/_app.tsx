5d7945927dcca8a14115db2ebaa8cd8ec1d8942c
강석현 <tjrgus0713@gmail.com>
2022-09-27 19:19:42 +0900

    🐛:: (#1) grey -> gray로 수정 및 theme style 사용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import ThemeStyle from '../styles';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeStyle>
+            <Component {...pageProps} />
+        </ThemeStyle>
+    );
 }
 
-export default MyApp
+export default MyApp;
