b07bd3b3a17f6a43b3351b419c2082ddac349335
강석현 <tjrgus0713@gmail.com>
2022-10-12 14:17:29 +0900

    🌧 :: (#8) icon 추가

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,3 +1,4 @@
 export { default as filterIcon } from './filter.svg';
 export { default as starIcon } from './star.svg';
 export { default as viewIcon } from './view.svg';
+export { default as heartIcon } from './heart.svg';
