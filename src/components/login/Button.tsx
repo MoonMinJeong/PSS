54fe10bbf27e24e02eb3607353bb3ad4ac91f7db
hyeyeonchurros <hyeyoenchurros@naver.com>
2022-10-23 23:19:32 +0900

    🌧 :: (#10) Login Modal 퍼블리싱

---
+++ src/components/login/Button.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+
+interface Props {
+    text: string;
+    img: string;
+    color: string;
+    backgroundColor: string;
+    onClick: () => void;
+}
+
+const Button = ({ text, img, color, backgroundColor, onClick }: Props) => {
+    return (
+        <ButtonWrapper backgroundColor={backgroundColor} onClick={onClick}>
+            <img src={img} />
+            <Text color={color}>{text}</Text>
+        </ButtonWrapper>
+    );
+};
+
+const ButtonWrapper = styled.div<{ backgroundColor: string }>`
+    width: 280px;
+    height: 48px;
+    border-radius: 8px;
+    background-color: ${({ backgroundColor }) => backgroundColor};
+    border: ${({ backgroundColor }) => backgroundColor === '#FFFFFF' && '1px solid #8D8D8D'};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 12px;
+    cursor: pointer;
+    > img {
+        width: 24px;
+        height: 24px;
+        margin-right: 20px;
+    }
+`;
+
+const Text = styled.p`
+    font-weight: 500;
+    font-size: 18px;
+    margin-top: 2px;
+    color: ${({ color }) => color};
+`;
+
+export default Button;
---
+++ src/components/login/LoginModal.tsx
@@ -0,0 +1,91 @@
+import styled from '@emotion/styled';
+import Button from './Button';
+import Github from '../../assets/login/github.png';
+import Google from '../../assets/login/google.png';
+import LoginBackGround from '../../assets/login/loginBackground.png';
+import CloseIcon from '../../assets/login/close.png';
+
+const LoginModal = () => {
+    return (
+        <_ModalBackground>
+            <_ModalContainer>
+                <_ImageWrapper />
+                <_ContentContainer>
+                    <h1>LOGIN</h1>
+                    <p>로그인 후 프소서의 서비스를 이용해보세요!</p>
+                    <Button
+                        text="Github로 로그인"
+                        img={Github.src}
+                        backgroundColor={'#000000'}
+                        color={'#FFFFFF'}
+                        onClick={() => {}}
+                    />
+                    <Button
+                        text="Google로 로그인"
+                        img={Google.src}
+                        backgroundColor={'#FFFFFF'}
+                        color={'#8D8D8D'}
+                        onClick={() => {}}
+                    />
+                </_ContentContainer>
+                <img src={CloseIcon.src} />
+            </_ModalContainer>
+        </_ModalBackground>
+    );
+};
+
+const _ModalBackground = styled.div`
+    background-color: rgba(0, 0, 0, 0.3);
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const _ImageWrapper = styled.div`
+    background-image: url(${LoginBackGround.src});
+    background-repeat: no-repeat;
+    background-size: contain;
+    width: 400px;
+`;
+
+const _ContentContainer = styled.div`
+    display: flex;
+    margin-top: 78px;
+    flex-direction: column;
+    align-items: center;
+    > h1 {
+        color: ${({ theme }) => theme.color.black};
+        font-weight: ${({ theme }) => theme.font.medium};
+        font-size: 40px;
+    }
+    > p {
+        font-size: 14px;
+        margin: 8px 0 24px;
+        color: ${({ theme }) => theme.color.gray700};
+    }
+`;
+
+const _ModalContainer = styled.div`
+    width: 800px;
+    height: 400px;
+    border-radius: 16px;
+    background-color: ${({ theme }) => theme.color.white};
+    box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+    display: flex;
+    overflow: hidden;
+    position: relative;
+    > img {
+        width: 40px;
+        height: 40px;
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        cursor: pointer;
+    }
+`;
+
+export default LoginModal;
