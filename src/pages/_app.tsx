2bc7cd81c3fb7f093dfd63bea72e047444dafc32
강석현 <tjrgus0713@gmail.com>
2022-10-25 08:37:58 +0900

    🌧 :: (#13) css파일 불러옴

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import ThemeStyle from '../styles';
+import 'rc-slider/assets/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
