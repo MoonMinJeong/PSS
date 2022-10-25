4a618e3e3f1255c2c861c8b4055ceffe459f5757
강석현 <tjrgus0713@gmail.com>
2022-10-25 11:52:40 +0900

    🌧 :: (#7) 헤더 추가

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -2,3 +2,4 @@ export { default as filterIcon } from './filter.svg';
 export { default as starIcon } from './star.svg';
 export { default as viewIcon } from './view.svg';
 export { default as heartIcon } from './heart.svg';
+export { default as arrowIcon } from './arrow.svg';
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from 'next/app';
 import ThemeStyle from '../styles';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeStyle>
+            <Header />
             <Component {...pageProps} />
         </ThemeStyle>
     );
