b9cc31fe6c99fba08efa047468308445f9a5479d
ipiyou <tao710803@gmail.com>
2022-11-16 23:57:20 +0900

    api 이미지 오류 해결

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ['maon.s3.ap-northeast-2.amazonaws.com'], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
