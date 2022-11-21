06903f8c0fd05f292060a4a856882247ce411789
hyeyeonchurros <hyeyoenchurros@naver.com>
2022-11-21 18:27:14 +0900

    🌧 :: (#34) Github oAuth 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,25 @@ import Filter from '../components/Filter';
 import styled from '@emotion/styled';
 import PostList from '../components/post/PostList';
 import Banner from '../components/Banner';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { getGithubUser } from '../apis/user';
 
 const MainPage = () => {
+    const id = useRouter().query;
+    useEffect(() => {
+        const user = async () => {
+            if (id.code) {
+                const data = await getGithubUser(id.code as string);
+                if (data) {
+                    localStorage.setItem('access_token', data.access_token);
+                    localStorage.setItem('refresh_token', data.refresh_token);
+                }
+            }
+        };
+        user();
+    }, [id]);
+
     return (
         <_Wrapper>
             <Banner />
