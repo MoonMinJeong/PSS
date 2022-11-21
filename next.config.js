7ac730d63ba7c45d13acf824f308c49cec575267
강석현 <tjrgus0713@gmail.com>
2022-11-21 19:26:45 +0900

    🐛 :: next.config 수정

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        domains: ['maon.s3.ap-northeast-2.amazonaws.com'], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
+        domains: ['maon.s3.ap-northeast-2.amazonaws.com', 'avatars.githubusercontent.com'], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
     },
 };
 
