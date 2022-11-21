6773ce008abe7eb07dbf805c7766c3d6d1716edc
강석현 <tjrgus0713@gmail.com>
2022-11-21 12:10:59 +0900

    🌧 :: (#35) 소개글 리스트 보기 연결

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,17 @@ import Filter from '../components/Filter';
 import styled from '@emotion/styled';
 import PostList from '../components/post/PostList';
 import Banner from '../components/Banner';
+import { useQuery } from 'react-query';
+import { getPostList } from '../apis/notice';
 
 const MainPage = () => {
+    const { data: postList } = useQuery(['postlist'], () => getPostList('like', 0, ''));
+    console.log(postList);
     return (
         <_Wrapper>
             <Banner />
             <Filter />
-            <PostList postList={[]} marginTop={40} />
+            <PostList postList={postList?.data.notice_list || []} marginTop={40} />
         </_Wrapper>
     );
 };
