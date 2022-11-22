8c37c66955448015ab93fcf16635618286eaa199
hyeyeonchurros <hyeyoenchurros@naver.com>
2022-11-22 12:20:47 +0900

    🐛 :: (#40) 댓글 답글 기능 수정

--- src/apis/reply.ts
+++ src/apis/reply.ts
@@ -2,9 +2,9 @@ import { ReplyRequest } from '../models/reply/request';
 import { instance } from './customAxios';
 
 export const writeReply = async (body: ReplyRequest, comment_id: string) => {
-    await instance.post(`/reply?comment-id=${comment_id}`, body);
+    await instance.post(`/reply/${comment_id}`, body);
 };
 
 export const deleteReply = async (reply_id: string) => {
-    await instance.delete(`/reply?reply-id=${reply_id}`);
+    await instance.delete(`/reply/${reply_id}`);
 };
