b6b66265926aeb8dd1cf10646625d9af1edfee47
hyeyeonchurros <hyeyoenchurros@naver.com>
2022-11-27 23:39:28 +0900

    🌧 :: 배너 추가

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import banner from '../assets/banner.png';
 
 const Banner = () => {
     return <_Wrapper></_Wrapper>;
@@ -8,5 +9,7 @@ export default Banner;
 const _Wrapper = styled.div`
     width: 100%;
     height: 500px;
-    background-color: ${({ theme }) => theme.color.gray500};
+    background-size: cover;
+    background-image: url(${banner.src});
+    background-repeat: no-repeat;
 `;
