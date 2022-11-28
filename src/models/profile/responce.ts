ae793081d1cbe46a9cd74032db5728369a8e2f80
ipiyou <tao710803@gmail.com>
2022-11-28 17:24:16 +0900

    conflict resolve

--- src/models/profile/responce.ts
+++ src/models/profile/responce.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { NoticeCardData } from "../notice/response";
-=======
-import { NoticeCardData } from '../notice/response';
->>>>>>> 18-pub-temp-post
 
 export interface GetPostResponce {
     notice_list: NoticeCardData[];
