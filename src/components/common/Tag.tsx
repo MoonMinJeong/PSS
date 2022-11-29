0f8533ffffc0dee92d4cafa1b02ef1711b18b4b7
hyeyeonchurros <hyeyoenchurros@naver.com>
2022-11-29 21:50:54 +0900

    Tag 수정

--- src/components/common/Tag.tsx
+++ src/components/common/Tag.tsx
@@ -8,12 +8,13 @@ const Tag = ({ text }: Props) => {
     return <_Wrapper>{text}</_Wrapper>;
 };
 
-const _Wrapper = styled.div`
+const _Wrapper = styled.p`
     background-color: ${({ theme }) => theme.color.main};
     color: ${({ theme }) => theme.color.white};
     font-size: 12px;
     font-weight: ${({ theme }) => theme.font.medium};
     padding: 4px 12px 3px;
+    white-space: nowrap;
     border-radius: 50px;
 `;
 
